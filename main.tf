45a3080134f01fd8e46a243459a532a6bd13e239
leehee <leehee@amazon.com>
2021-09-29 21:39:01 +0900

    add environment

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+    backend "s3" {
+        bucket = "iac-cicd-terraform"
+        key = "terraform/backend"
+        region = "ap-northeast-2"
+    }
+}
+
+locales {
+    env_name = "sandbox"
+    aws_region = "ap-northeast-2"
+    k8s_cluster_name = "ms-cluster"
+}
+
+# Networkd Configruations
+
+# EKS Configruations
+
+# Git Ops Configruations
